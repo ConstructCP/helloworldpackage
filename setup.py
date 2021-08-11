5addb7c526940389575591a3e08589506ef50a90
Andrei Hubarevich <ahubarevich@ibagroup.eu>
2021-08-11 19:52:43 +0300

    add version to setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ import setuptools
 setuptools.setup(
     name="helloworldpackage",
     description="Very simple package just to test github CI ",
+    version="0.0.1",
     package_dir={"": "src"}
 )
